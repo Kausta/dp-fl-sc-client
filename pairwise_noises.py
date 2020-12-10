2c377a828ef49603cfbfed5604a242faa33a7513
utkn <39ph4f87a@gmail.com>
2020-12-10 23:11:07 +0300

    Updating noises (for now using insecure PRG)

--- pairwise_noises.py
+++ pairwise_noises.py
@@ -1,14 +1,19 @@
 from pyDH import DiffieHellman, primes
+import random
 
 class PairwiseNoises:
-
     dh_instances: dict = None
     noise_map: dict = {}
     # Store the contributions that were received before the generation of private keys.
     unassigned_contributions: dict = {}
+    # PRGs
+    prgs: dict = {}
     p = None
+    max_pairs: int = 0
 
+    # Invoked by FedClient during setup.
     def generate_private_keys(self, group_desc: int, clients: dict):
+        self.max_pairs = len(clients)
         # Create Diffie Hellman instances for every other client.
         self.dh_instances = {client_id: DiffieHellman(group_desc) for client_id in clients.keys()}
         # Set the prime.
@@ -19,6 +24,7 @@ class PairwiseNoises:
         # Clear the unassigned contributions.
         self.unassigned_contributions = {}
 
+    # Invoked by FedClient during setup.
     def get_public_keys(self, clients: dict):
         public_keys = {client_id: self.dh_instances[client_id].gen_public_key() for client_id in clients.keys()}
         return public_keys
@@ -33,21 +39,29 @@ class PairwiseNoises:
             # If the private keys were generated, calculate the pairwise noise.
             dh = self.dh_instances[contributor_id]
             self.noise_map[contributor_id] = dh.gen_shared_key(contribution)
+        # If we have calculated all possible noises, initialize the PRGs.
+        if len(self.noise_map) == self.max_pairs:
+            self.initialize_prgs()
     
+    def initialize_prgs(self):
+        print("PairwiseNoises: Initializing PRGs...")
+        self.prgs = {client_id: random.Random(shared_noise) for client_id, shared_noise in self.noise_map.items()}
+        self.update_noises()
+    
+    # Updates the noises.
     def update_noises(self):
-        pass
+        print("PairwiseNoises: Updating noises...")
+        for client_id in self.noise_map.keys():
+            self.noise_map[client_id] = self.prgs[client_id].randint(0, self.p)
 
     def get_noise(self, self_id: int):
         smaller_ids = filter(lambda k, v: k < self_id, self.noise_map.items())
         larger_ids = filter(lambda k, v: k > self_id, self.noise_map.items())
-
         neg_noises = map(lambda k, v: v, smaller_ids)
         pos_noises = map(lambda k, v: v, larger_ids)
-
         acc = 0
         for n in pos_noises:
             acc = (acc + n) % self.p
         for n in neg_noises:
             acc = (acc - n) % self.p
-
         return acc
